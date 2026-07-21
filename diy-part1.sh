0f799292fbe2aa7277f3e250fdef02ad55d3eec9
HASH512 <hash513@outlook.com>
2026-07-21 19:51:41 +0800

    edited

--- diy-part1.sh
+++ diy-part1.sh
@@ -27,3 +27,5 @@ git clone https://github.com/eamonxg/luci-theme-aurora package/luci-theme-aurora
 git clone https://github.com/eamonxg/luci-app-aurora-config package/luci-app-aurora-config
 git clone https://github.com/timsaya/luci-app-bandix package/luci-app-bandix
 git clone https://github.com/timsaya/openwrt-bandix package/openwrt-bandix
+echo 'src-git qmodem https://github.com/FUjr/QModem.git;main' >> feeds.conf.default
+git clone https://github.com/FUjr/QModem.git package/qmodem
